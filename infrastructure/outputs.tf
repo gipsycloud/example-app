fb82ab0030c135e5d67cb9ab9377f64ed47b1e09
harryd <harry@Wais-MacBook-Pro.local>
2024-11-23 10:11:55 +0000

    Separated terraform resources

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,9 +1,9 @@
-output "public_ip" {
-  value       = aws_instance.web.public_ip
-  description = "The public IP address of the ec2 instance"
+output "frontend_ip" {
+  value       = "http://${aws_instance.frontend.public_ip}"
+  description = "The public IP address of the frontend instance"
 }
 
-output "hello_endpoint" {
-  value       = "http://${aws_instance.web.public_ip}/api/v1/hello"
+output "backend_endpoint" {
+  value       = "http://${aws_instance.backend.public_ip}:3000/api/v1/hello"
   description = "The hello endpoint of the express.js application"
 }
