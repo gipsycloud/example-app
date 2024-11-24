3eaf289329f6d142c9f1b1a0abb056b8ef9c9f3a
harryd <harry@Wais-MacBook-Pro.local>
2024-11-24 10:53:32 +0000

    Changed hello to helloworld

--- express-backend/index.js
+++ express-backend/index.js
@@ -6,7 +6,7 @@ const port = 3000;
 app.use(cors());
 
 app.get("/api/v1/hello", (req, res) => {
-  res.json({ message: "hello" });
+  res.json({ message: "hello, world!" });
 });
 
 // default catch-all router
