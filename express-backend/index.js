1237e0f7cf37581af0b09ee9d38fea35666fd895
harryd <harry@Wais-MacBook-Pro.local>
2024-11-25 08:03:04 +0000

    Updated unit test case

--- express-backend/index.js
+++ express-backend/index.js
@@ -6,7 +6,7 @@ const port = 3000;
 app.use(cors());
 
 app.get("/api/v1/hello", (req, res) => {
-  res.json({ message: "hello, world!" });
+  res.json({ message: "hello, changed!" });
 });
 
 // default catch-all router
