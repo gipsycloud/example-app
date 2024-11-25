49c6b2c57c2522b95984b4f4ccbb44afd4086be9
harryd <harry@Wais-MacBook-Pro.local>
2024-11-25 11:12:39 +0000

    Added new terraform resources

--- express-backend/index.test.js
+++ express-backend/index.test.js
@@ -2,10 +2,10 @@ const request = require("supertest");
 const { app, server } = require("./index");
 
 describe("GET /api/v1/hello", () => {
-  test('should return a JSON response with the message "hello, changed!"', async () => {
+  test('should return a JSON response with the message "hello, discord!"', async () => {
     const response = await request(app).get("/api/v1/hello").expect(200);
 
-    expect(response.body.message).toBe("hello, changed!");
+    expect(response.body.message).toBe("hello, discord!");
   });
 
   test('should not return a JSON response with the message "hi"', async () => {
--- infrastructure/load-balancer.tf
+++ infrastructure/load-balancer.tf
@@ -1,51 +1,16 @@
 resource "aws_lb" "lb" {
-  name               = "hello-discord"
+  name               = "backend-lb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.lb.id]
-  subnets            = data.aws_subnets.public.ids
+  security_groups = [
+    aws_security_group.lb.id
+  ]
+  subnets = data.aws_subnets.public.ids
 
   enable_deletion_protection = false
 
   tags = {
     "lab" = "example"
-  }
-}
-
-resource "aws_lb_target_group" "backend" {
-  name     = "backend-group"
-  port     = 3000
-  protocol = "HTTP"
-  vpc_id   = data.aws_vpc.default.id
-
-  health_check {
-    enabled             = true
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    timeout             = 5
-    interval            = 6
-    path                = "/api/v1/hello"
-  }
-}
-
-resource "aws_lb_target_group_attachment" "backend_register" {
-  target_group_arn = aws_lb_target_group.backend.arn
-  target_id        = aws_instance.backend.id
-  port             = 3000
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_lb_listener" "backend" {
-  load_balancer_arn = aws_lb.lb.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.backend.arn
   }
 }
 
@@ -59,7 +24,7 @@ resource "aws_security_group" "lb" {
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+resource "aws_vpc_security_group_ingress_rule" "lb_allow_http_80" {
   security_group_id = aws_security_group.lb.id
   cidr_ipv4         = "0.0.0.0/0"
 
@@ -68,7 +33,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_http" {
   to_port     = 80
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_https" {
+resource "aws_vpc_security_group_ingress_rule" "lb_allow_http_443" {
   security_group_id = aws_security_group.lb.id
   cidr_ipv4         = "0.0.0.0/0"
 
@@ -77,9 +42,36 @@ resource "aws_vpc_security_group_ingress_rule" "allow_https" {
   to_port     = 443
 }
 
-resource "aws_vpc_security_group_egress_rule" "allow_egress_lb_all" {
+resource "aws_vpc_security_group_egress_rule" "lb_allow_egress_all" {
   security_group_id = aws_security_group.lb.id
   cidr_ipv4         = "0.0.0.0/0"
 
   ip_protocol = -1
+}
+
+resource "aws_lb_target_group" "backend" {
+  name     = "backend"
+  port     = 3000
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    interval            = 6
+    timeout             = 5
+    path                = "/api/v1/hello"
+  }
+}
+
+resource "aws_lb_listener" "backend" {
+  load_balancer_arn = aws_lb.lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.backend.arn
+  }
 }
