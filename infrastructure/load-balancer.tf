d3beb8c289fcd8820f2b8256dfb80c692902c9b6
harryd <harry@Wais-MacBook-Pro.local>
2024-11-26 08:35:51 +0000

    Added frontend target gp

--- infrastructure/load-balancer.tf
+++ infrastructure/load-balancer.tf
@@ -42,6 +42,15 @@ resource "aws_vpc_security_group_ingress_rule" "lb_allow_http_443" {
   to_port     = 443
 }
 
+resource "aws_vpc_security_group_ingress_rule" "lb_allow_http_3000" {
+  security_group_id = aws_security_group.lb.id
+  cidr_ipv4         = "0.0.0.0/0"
+
+  ip_protocol = "tcp"
+  from_port   = 3000
+  to_port     = 3000
+}
+
 resource "aws_vpc_security_group_egress_rule" "lb_allow_egress_all" {
   security_group_id = aws_security_group.lb.id
   cidr_ipv4         = "0.0.0.0/0"
@@ -69,11 +78,40 @@ resource "aws_lb_target_group" "backend" {
 
 resource "aws_lb_listener" "backend" {
   load_balancer_arn = aws_lb.lb.arn
-  port              = "80"
+  port              = "3000"
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.backend.arn
+  }
+}
+
+resource "aws_lb_target_group" "frontend" {
+  name     = "frontend"
+  port     = 3000
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    unhealthy_threshold = 10
+    interval            = 30
+    timeout             = 10
+    path                = "/"
+  }
+
+  deregistration_delay = 10
+}
+
+resource "aws_lb_listener" "frontend" {
+  load_balancer_arn = aws_lb.lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.frontend.arn
   }
 }
