79675e9082007f01d8a6481d15cadd06f3eca4b3
harryd <harry@Wais-MacBook-Pro.local>
2024-11-26 10:42:19 +0000

    Modified healthcheck interval/timeout

--- infrastructure/load-balancer.tf
+++ infrastructure/load-balancer.tf
@@ -68,8 +68,8 @@ resource "aws_lb_target_group" "backend" {
     enabled             = true
     healthy_threshold   = 2
     unhealthy_threshold = 10
-    interval            = 6
-    timeout             = 5
+    interval            = 30
+    timeout             = 10
     path                = "/api/v1/hello"
   }
 
