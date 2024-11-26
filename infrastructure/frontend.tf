e271b4324068fd84cc6ca51489e036302e3afcdd
harryd <harry@Wais-MacBook-Pro.local>
2024-11-26 08:42:17 +0000

    Removed disabling public ip

--- infrastructure/frontend.tf
+++ infrastructure/frontend.tf
@@ -13,18 +13,18 @@ resource "aws_launch_template" "frontend" {
 
   instance_type = "t2.micro"
 
-#   vpc_security_group_ids = [
-#     aws_security_group.frontend.id,
-#     aws_security_group.ssh.id,
-#   ]
+  vpc_security_group_ids = [
+    aws_security_group.frontend.id,
+    aws_security_group.ssh.id,
+  ]
 
-  network_interfaces {
-    associate_public_ip_address = false
-    security_groups = [
-      aws_security_group.frontend.id,
-      aws_security_group.ssh.id,
-    ]
-  }
+#   network_interfaces {
+#     associate_public_ip_address = false
+#     security_groups = [
+#       aws_security_group.frontend.id,
+#       aws_security_group.ssh.id,
+#     ]
+#   }
 
   tag_specifications {
     resource_type = "instance"
