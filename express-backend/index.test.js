5f7e2d5dea137b5e096639d64dc1fedad6c8ea5e
harryd <harry@Wais-MacBook-Pro.local>
2024-11-26 10:30:32 +0000

    Switched to migalapa

--- express-backend/index.test.js
+++ express-backend/index.test.js
@@ -2,10 +2,10 @@ const request = require("supertest");
 const { app, server } = require("./index");
 
 describe("GET /api/v1/hello", () => {
-  test('should return a JSON response with the message "hello, discord!"', async () => {
+  test('should return a JSON response with the message "Mingalapa!"', async () => {
     const response = await request(app).get("/api/v1/hello").expect(200);
 
-    expect(response.body.message).toBe("hello, discord!");
+    expect(response.body.message).toBe("Mingalapa!");
   });
 
   test('should not return a JSON response with the message "hi"', async () => {
