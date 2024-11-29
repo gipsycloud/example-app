1e608a9c73ceac21bcb92c389eceb724ea78d7ba
harryd <harry@Wais-MacBook-Pro.local>
2024-11-29 10:41:55 +0000

    Changed to nin hao

--- express-backend/index.js
+++ express-backend/index.js
@@ -6,7 +6,7 @@ const port = 3000;
 app.use(cors());
 
 app.get("/api/v1/hello", (req, res) => {
-  res.json({ message: "Mingalapa!" });
+  res.json({ message: "Nin Hao!" });
 });
 
 // default catch-all router
