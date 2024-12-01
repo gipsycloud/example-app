53272bce384b4d71f9d1bac20dc6135c840b1912
harryd <harry@Wais-MacBook-Pro.local>
2024-12-01 16:05:04 +0000

    Added more unit tests

--- express-backend/index.js
+++ express-backend/index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 const port = 3000;
 const { pool, ping } = require("./db");
+const { body, validationResult } = require("express-validator");
 
 app.use(cors());
 app.use(express.json());
@@ -25,31 +26,58 @@ app.get("/api/v1/users", async (req, res) => {
   }
 });
 
-app.post("/api/v1/users", async (req, res) => {
-  try {
-    const { name, email } = req.body;
-    const result = await pool.query(
-      "INSERT INTO users (name, email) VALUES ($1, $2) RETURNING id",
-      [name, email]
-    );
-    res.status(201).json({
-      id: result.rows[0].id,
-      name: name,
-      email: email,
-    });
-  } catch (error) {
-    if (error.constraint === "users_email_key") {
-      res.status(400).json({
-        error: "Email already exists. Please use a different email.",
+app.post(
+  "/api/v1/users",
+  [
+    body("name")
+      .notEmpty({ ignore_whitespace: true })
+      .withMessage("name must be provided")
+      .isLength({ min: 5, max: 20 })
+      .withMessage("name must be between 5 and 20 bytes"),
+    body("email")
+      .notEmpty()
+      .withMessage("email must be provided")
+      .isEmail()
+      .withMessage("email must be valid")
+      .isLength({ max: 30 })
+      .withMessage("email must not exceed 30 bytes"),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (errors.errors.length !== 0) {
+        return res.status(400).json({
+          errors: errors.array().map((err) => {
+            return {
+              field: err.path,
+              message: err.msg,
+            };
+          }),
+        });
+      }
+      const { name, email } = req.body;
+
+      const result = await pool.query(
+        "INSERT INTO users (name, email) VALUES ($1, $2) RETURNING id",
+        [name, email]
+      );
+      res.status(201).json({
+        id: result.rows[0].id,
+        name: name,
+        email: email,
       });
-    } else {
-      console.error(error.message);
-      res.status(500).json({
-        error: "Internal server error",
-      });
+    } catch (error) {
+      if (error.constraint === "users_email_key") {
+        res.status(400).json({
+          error: "Email already exists. Please use a different email.",
+        });
+      } else {
+        console.error(error.message);
+        res.status(500).json({ error: "Internal server error" });
+      }
     }
   }
-});
+);
 
 // default catch-all router
 app.use((req, res) => {
